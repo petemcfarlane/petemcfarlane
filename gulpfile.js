7d3474e8b340d3d7b725042cc444dad4b02c5547
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-04-12 17:56:51 +0100

    Added bootstrap, gulp, npm, updated theme.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cleanCss = require('gulp-clean-css');
+
+gulp.task('default', ['sass', 'bootstrap-js']);
+
+gulp.task('bootstrap-js', function () {
+    return gulp.src('node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js')
+        .pipe(gulp.dest('output_dev/js/'))
+        .pipe(gulp.dest('output_prod/js/'))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('source/_sass/style.sass')
+        .pipe(sass())
+        .pipe(cleanCss())
+        .pipe(gulp.dest('output_dev/css/'))
+        .pipe(gulp.dest('output_prod/css/'))
+});
--- publish.sh
+++ publish.sh
@@ -7,6 +7,9 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
+# currently copy js and compile sass (might make this into a sculpin bundle...)
+gulp
+
 # # Add --delete right before "output_prod" to have rsync remove files that are
 # # deleted locally from the destination too. See README.md for an example.
 rsync -avz --delete output_prod/ pete@petemcfarlane.com:/var/www/petemcfarlane.com
