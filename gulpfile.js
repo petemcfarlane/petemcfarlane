d38b1f015293bbea832ce6bdd14bd312c6588065
Pete McFarlane <peterjohnmcfarlane@gmail.com>
2016-04-14 10:21:35 +0100

    Dynamically add categories and tags in the sidebar with AJAX call.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp'),
     sass = require('gulp-sass'),
-    cleanCss = require('gulp-clean-css');
+    cleanCss = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    del = require('del');
 
-gulp.task('default', ['sass', 'bootstrap-js']);
+gulp.task('default', ['sass', 'bootstrap-js', 'categories-and-tasks-to-json']);
 
 gulp.task('sculpin', ['default']);
 
@@ -18,4 +20,26 @@ gulp.task('sass', function () {
         .pipe(cleanCss())
         .pipe(gulp.dest('output_dev/css/'))
         .pipe(gulp.dest('output_prod/css/'))
+});
+
+gulp.task('categories-and-tasks-to-json', ['copy-categories-and-tasks-to-json', 'delete-old-categories-and-tasks-html-folder']);
+
+gulp.task('copy-categories-and-tasks-to-json', function () {
+   gulp.src('output_dev/categories-and-tags/index.html')
+       .pipe(rename('categories-and-tags.json'))
+       .pipe(gulp.dest('output_dev/'));
+   gulp.src('output_prod/categories-and-tags/index.html')
+       .pipe(rename('categories-and-tags.json'))
+       .pipe(gulp.dest('output_prod/'));
+});
+
+gulp.task('delete-old-categories-and-tasks-html-folder', ['copy-categories-and-tasks-to-json'], function () {
+    return del([
+        'output_dev/categories-and-tags/**',
+        'output_dev/_posts/**',
+        'output_dev/_sass/**',
+        'output_prod/categories-and-tags/**',
+        'output_prod/_posts/**',
+        'output_prod/_sass/**'
+    ])
 });
